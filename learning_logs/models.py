5dcf78ade38d1a4de9027a74d1e9897e0ae46ce4
ealalin <alain_linard@hotmail.com>
2023-12-18 00:27:37 +0100

    correction from the 18/12/2023

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,38 +1,54 @@
-# from django.db import models 
-#
-# class Topic(models.Model):
-#   # a topic that the user is learning about
-#     topic1 = models.ForeignKey("Topic", on_delete=models.CASCADE)
-#     text = models.TextField(max_length=200)
-#     date_added =  models.DateTimeField(auto_now_add=True) 
-# class Entry(models.Model):
-#     topic = models.ForeignKey(Topic, on_delete=models.CASCADE) 
-#     text = models.CharField(max_length=200)  
-#     date_added = models.DateTimeField(auto_now_add=True)
-# class Meta:
-#         verbose_name_plural = 'entries'
-# def __str__(self):
-#         """ return a string representing the model."""
-#         return self.text[:50] + "..."
-#
-#*********** original ***********
 from django.db import models
+from django.utils import timezone
+
+class TextField(models.TextField):
+    pass
+
 class Topic(models.Model):
     # a topic that the user is learning about
-    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    #date_added = models.DateTimeField(auto_now_add=True, default=timezone.now)
 
     def __str__(self):
-        return self.name
+        """Return a string representing the model."""
+        return f"Topic {self.id}"
 
 class Entry(models.Model):
-    #topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = TextField(max_length=200)  # Using the custom TextField
     date_added = models.DateTimeField(auto_now_add=True)
+    #date_added = models.DateTimeField(auto_now_add=True, default=timezone.now)
+
 
     class Meta:
         verbose_name_plural = 'entries'
 
+    #def __str__(self):
+    #    """Return a string representing the model."""
+    #    return self.text[:50] + "..."
+    
     def __str__(self):
-        return self.text[:50] + "..." 
+        """Return a string representing the model."""
+        return f"Topic {self.id}"
+
+#*********** original ***********
+# from django.db import models
+# class Topic(models.Model):
+#     # a topic that the user is learning about
+#     name = models.CharField(max_length=200)
+
+#     def __str__(self):
+#         return self.name
+
+# class Entry(models.Model):
+#     #topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+#     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         verbose_name_plural = 'entries'
+
+#     def __str__(self):
+#         return self.text[:50] + "..." 
 
 # ************* end Original *********
