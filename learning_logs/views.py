75ffe9baf10383c0e837dfc6ba3375e70d737cdf
ealalin <alain_linard@hotmail.com>
2024-01-11 17:43:22 +0100

    resolve the Topic_list

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,25 +1,24 @@
 """ From learning_log/learning_logs """
 from django.shortcuts import render  
 from django.shortcuts import get_object_or_404
-from .models import Topic
-# import  get_object_or_494
+from .models import Topic  # Import the Topic model
 # Create your views here.
 
 def index(request):
     """ the home page from learning_log """
     return render(request, 'learning_logs/index.html')
 
+#def topics(request):
+#    """show alle topic"""
+#    topics = topics.objects.order_by('date_added')   #date_added
+#    context = {'topics':topics}
+#    return render(request, 'learning_logs/topics.html', context)
 def topics(request):
-    """show alle topic"""
-    topics = Topic.objects.order_by('date_added')   #date_added
-    context = {'topics':topics}
+    """show all topics"""
+    topics_list = Topic.objects.order_by('date_added')  # Use a different variable name
+    context = {'topics': topics_list}
     return render(request, 'learning_logs/topics.html', context)
-    # """Show a single topic """
-    # topic = Topic.objects.get(id=topic_id)
-    # entries = topic.entry_set.order_by('-date_added')
-    # context = {'topic':topic, 'entries': entries}
-    #return render(request,'learning_logs/topics.html', context)
-
+    
 #-----------------------------------------------
 # def topics(request):
 #      """Show all topics."""
@@ -27,12 +26,12 @@ def topics(request):
 #      context = {'topics': topics}
 #      return render(request, 'learning_logs/topics.html', context)
 
-def topic(request, topic_id):
-     """Show a single topic with its entries."""
-     topic = get_object_or_404(Topic, id=topic_id)
-     entries = topic.entry_set.order_by('-date_added')
-     context = {'topic': topic, 'entries': entries}
-     return render(request, 'learning_logs/topic.html', context)
+#def topic(request, topic_id):
+    """Show a single topic with its entries."""
+    topic = get_object_or_404(Topic, id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
 
 
 
