4fd1a19ab7158d41bae3f672a5e89be81959bbb0
ealalin <alain_linard@hotmail.com>
2024-01-13 08:30:22 +0100

    still need to correct error at Topics/1

---
+++ Intel/GfxCPLBatchFiles/{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat
@@ -0,0 +1,4 @@
+@echo off
+TIMEOUT 1 /NOBREAK
+if exist "C:\WINDOWS\System32\DriverStore\FileRepository\cui_dch.inf_amd64_92cf9d9d84f1d3db\igfxEM.exe" start "" "C:\WINDOWS\System32\DriverStore\FileRepository\cui_dch.inf_amd64_92cf9d9d84f1d3db\igfxEM.exe"
+del /Q "C:\Intel\GfxCPLBatchFiles\{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat"
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,30 +1,20 @@
 """ From learning_log/learning_logs """
 from django.shortcuts import render  
 from django.shortcuts import get_object_or_404
-from .models import Topic  # Import the Topic model
+from learning_logs.models import Topic  # Import the Topic model
 # Create your views here.
 
 def index(request):
     """ the home page from learning_log """
     return render(request, 'learning_logs/index.html')
 
-#def topics(request):
-#    """show alle topic"""
-#    topics = topics.objects.order_by('date_added')   #date_added
-#    context = {'topics':topics}
-#    return render(request, 'learning_logs/topics.html', context)
+
 def topics(request):
     """show all topics"""
     topics_list = Topic.objects.order_by('date_added')  # Use a different variable name
     context = {'topics': topics_list}
     return render(request, 'learning_logs/topics.html', context)
     
-#-----------------------------------------------
-# def topics(request):
-#      """Show all topics."""
-#      topics = Topic.objects.all()
-#      context = {'topics': topics}
-#      return render(request, 'learning_logs/topics.html', context)
 
 #def topic(request, topic_id):
     """Show a single topic with its entries."""
