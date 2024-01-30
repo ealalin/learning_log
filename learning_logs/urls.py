cee6b40160bb3838c9a1d66f6d0740c921753312
ealalin <alain_linard@hotmail.com>
2024-01-31 00:02:04 +0100

    version from the 31-01-2024

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,6 +10,12 @@ urlpatterns = [
     # show all topics
     url(r'^topics/$', views.topics, name='topics'),
     # detail page for a single topic
-    url(r'^topics/(?P<topic_id>\d+)/$' , views.Topic , name='topic'),      
+    url(r'^topic/(?P<topic_id>\d+)/$' , views.topic , name='topic'),  
+    # page for additing a new topic
+    url(r'^new_topic/$' , views.new_topic , name='new_topic'), 
+    # new_entry    
+    url(r'^new_entry/(?P<topic_id>\d+)/$' , views.new_entry , name='new_entry'), 
+     # test    
+     url(r'^test/$', views.test, name='test'),    
 ]
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,12 @@
 """ From learning_log/learning_logs """
 from django.shortcuts import render  
 from django.shortcuts import get_object_or_404
-from learning_logs.models import Topic  # Import the Topic model
+from .models import Topic , Entry  # Import the Topic model 
+#from .models import Entry  # Import the Entry model
+from django.http import HttpResponseRedirect
+#from django.core.urlresolvers import reverse 
+from django.urls import reverse
+from .forms import TopicForm, EntryForm
 # Create your views here.
 
 def index(request):
@@ -11,17 +16,67 @@ def index(request):
 
 def topics(request):
     """show all topics"""
-    topics_list = Topic.objects.order_by('date_added')  # Use a different variable name
-    context = {'topics': topics_list}
+    topics = Topic.objects.order_by('date_added')  # Use a different variable name
+    context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
-    
 
-#def topic(request, topic_id):
-    """Show a single topic with its entries."""
+# ------test to see the entry----    
+def test(request):
+    entries = Entry.objects.all()
+    topics = Topic.objects.all()
+    return render(request, 'learning_logs/topics.html', {'entries': entries, 'topics': topics})
+# --------end test -------------------------
+
+
+"""Show a single topic with its entries."""    
+from django.http import Http404
+def topic(request, topic_id):
+ try: 
     topic = get_object_or_404(Topic, id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
+    entries = topic.entry_set.order_by('date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+ except Topic.DoesNotExist:
+        # Raise a custom Http404 exception
+    raise Http404("Topic does not exist")
 
 
+def new_topic(request):
+    """ Add a new Topic""" 
+    if request.method != 'POST' :
+          # no data submitted ;create a blank form.
+          form = TopicForm()
+    else:  
+        # post data submitted ,process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+           form.save()
+           return HttpResponseRedirect(reverse('learning_logs:topics')) 
 
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+    
+#++++++++ end New proposal when we want to add a new entrey and the topic_id does not exist yet ex:http://127.0.0.1:8000/new_entry/25/
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, reverse
+from .models import Topic
+from .forms import EntryForm  # Import your EntryForm
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic"""
+    topic = get_object_or_404(Topic, id=topic_id)
+
+    if request.method == 'POST':
+        form = EntryForm(request.POST)  # Use request.POST to bind POST data to the form
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    else:
+        form = EntryForm()  # Create an empty form if it's a GET request
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
